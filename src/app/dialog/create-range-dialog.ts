beb9c65e1e1513ae43c384423112fa52c47e412b
antoine_galataud <antoine@foobot.io>
2025-06-17 16:33:18 +0200

    further cleanup

--- src/app/dialog/create-range-dialog.ts
+++ src/app/dialog/create-range-dialog.ts
@@ -1,47 +1,49 @@
-import { Component, Input, Output, EventEmitter, OnInit, OnDestroy } from "@angular/core";
+import {Component, Input, Output, EventEmitter, OnInit, OnDestroy} from '@angular/core';
 
 @Component({
-    selector:'create-range-dialog',
-    templateUrl:'create-range-dialog.html'
+  selector: 'create-range-dialog',
+  templateUrl: 'create-range-dialog.html'
 })
-export class CreateRangeDialog implements OnInit,OnDestroy {
-   
-    @Input()
-    display:boolean;
+export class CreateRangeDialog implements OnInit, OnDestroy {
 
-    @Output()
-    onSave:EventEmitter<any> = new EventEmitter<any>();
+  @Input()
+  display: boolean;
 
-    @Output()
-    onClose:EventEmitter<any> = new EventEmitter<any>();
+  @Output()
+  onSave: EventEmitter<any> = new EventEmitter<any>();
 
-    @Input()
-    range:any;
+  @Output()
+  onClose: EventEmitter<any> = new EventEmitter<any>();
 
-    constructor(){}
+  @Input()
+  range: any;
 
-    ngOnDestroy(): void {
-        this.onSave.unsubscribe();
+  constructor() {
+  }
+
+  ngOnDestroy(): void {
+    this.onSave.unsubscribe();
+  }
+
+  ngOnInit(): void {
+    if (!this.range) {
+      this.range = {};
     }
-    ngOnInit(): void {
-        if(!this.range) {
-            this.range = {};
-        }
-    }
+  }
 
-    onSaveClick():void {
-        if(this.range.start && this.range.end) {
-            if(this.range.start > this.range.end) {
-                throw new Error('start date cant be after end date!');
-            }
-        }
-        if((!this.range.start) || (!this.range.end)) {
-            throw new Error('Start date and End date must be set');
-        }
-        this.onSave.emit(this.range);   
+  onSaveClick(): void {
+    if (this.range.start && this.range.end) {
+      if (this.range.start > this.range.end) {
+        throw new Error('start date cant be after end date!');
+      }
     }
+    if ((!this.range.start) || (!this.range.end)) {
+      throw new Error('Start date and End date must be set');
+    }
+    this.onSave.emit(this.range);
+  }
 
-    onCloseClick():void {
-        this.onClose.emit();
-    }
+  onCloseClick(): void {
+    this.onClose.emit();
+  }
 }
